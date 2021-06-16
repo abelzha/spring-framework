47a5af32d0b63ad28cc59b97256bb6e6987a1671
junzhang27 <junzhang27@iflytek.com>
2021-06-16 18:53:58 +0800

    添加事务demo

--- demo1/src/main/java/transactiondemo/UserDao.java
+++ demo1/src/main/java/transactiondemo/UserDao.java
@@ -17,7 +17,7 @@ public class UserDao {
 
 	public void insertUser(){
 		String sql = "INSERT INTO abel_demo.test11 (username,password,phone,email) VALUES \n" +
-				"('client','$2a$10$5PEF2ekqYq0iu7VBYiH8BON434.nTe9doHD/O1lebhyhG8ywLyE9W','15888888888','lee.lusifer@gmail.com')";
+				"('client','$2a$10$5PEF2ekqYq0iu7VBYiH8BON434.nTe9doHD/O1lebhyhG8ywLyE9W','15888888888','lee@gmail.com')";
 		jdbcTemplate.update(sql);
 	}
 
